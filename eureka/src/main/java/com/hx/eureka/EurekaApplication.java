c423007297d20b77a7176a9d7e5b109d522b4c12
hx1098 <1098269542@qq.com>
2021-08-12 07:30:23 +0800

    eureka初始化

---
+++ eureka/src/main/java/com/hx/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.hx.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author hx
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
